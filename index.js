05803a0903145fddae321f1e14e5dfe5e5d90451
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-03-27 17:11:22 +0200

    Retrive a json object all the pages of the db

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import { Client } from "@notionhq/client"
+import { Client } from "@notionhq/client";
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
-const databaseId = process.env.NOTION_DATABASE_ID
-
-async function getDB() {
-  try {
-    const response = await notion.databases.retrieve({ database_id: databaseId }) 
-    console.log(response)
-    console.log("Success!")
-  } catch (error) {
-    console.error(error.body)
-  }
-}
-
-getDB()
+(async () => {
+  const databaseId = process.env.NOTION_DATABASE_ID;
+  const response = await notion.databases.query({
+    database_id: databaseId,
+    sorts: [
+      {
+        property: "Term",
+        direction: "ascending",
+      },
+    ],
+  });
+})();
 
